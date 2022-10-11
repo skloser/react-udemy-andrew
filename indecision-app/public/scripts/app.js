4d190a514c008142057e8f1207264640e5de3b10
Krum Bagashev <Krum.Bagashev@pharmalex.com>
2022-10-11 19:19:19 +0300

    Added yarn and babel Initial setup

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("App.js is running!");
+
+var template = React.createElement(
+  "h1",
+  null,
+  "Indicision App"
+);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
