612c0a171a0e2e0051ebf6755ea4ec454704a17e
Krum Bagashev <Krum.Bagashev@pharmalex.com>
2022-11-10 11:23:00 +0200

    Finalized styles Added media queries and favicon

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -6,10 +6,16 @@ const OptionModal = (props) => (
 		isOpen={!!props.selectedOption}
 		contentLabel="Selected Option"
 		onRequestClose={props.handleClearSelectedOption}
-		ariaHideApp={false}>
-		<h3>Selected Option</h3>
-		{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.handleClearSelectedOption}>Okay</button>
+		ariaHideApp={false}
+		closeTimeoutMS={200}
+		className="modal">
+		<h3 className="modal__title">Selected Option</h3>
+		{props.selectedOption && (
+			<p className="modal__body">{props.selectedOption}</p>
+		)}
+		<button className="button" onClick={props.handleClearSelectedOption}>
+			Okay
+		</button>
 	</Modal>
 );
 
