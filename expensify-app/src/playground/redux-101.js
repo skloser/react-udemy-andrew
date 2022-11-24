49d83923a50aedb29b91758b9aa2c0ab0dd277ab
Krum Bagashev <krum.bagashev@gmail.com>
2022-11-24 23:39:32 +0200

    Added reducers

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,18 +1,33 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }, action) => {
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+	type: "INCREMENT",
+	incrementBy,
+});
+
+const decrementCount = ({ decrementBy = 1 }) => ({
+	type: "DECREMENT",
+	decrementBy,
+});
+
+const setCount = ({ count = 0 }) => ({
+	type: "SET",
+	count,
+});
+
+const resetCount = () => ({
+	type: "RESET",
+});
+
+const countReducer = (state = { count: 0 }, action) => {
 	switch (action.type) {
 		case "INCREMENT":
-			const incrementBy =
-				typeof action.incrementBy === "number" ? action.incrementBy : 1;
 			return {
-				count: state.count + incrementBy,
+				count: state.count + action.incrementBy,
 			};
 		case "DECREMENT":
-			const decrementBy =
-				typeof action.decrementBy === "number" ? action.decrementBy : 1;
 			return {
-				count: state.count - decrementBy,
+				count: state.count - action.decrementBy,
 			};
 		case "SET":
 			return {
@@ -25,31 +40,20 @@ const store = createStore((state = { count: 0 }, action) => {
 		default:
 			return state;
 	}
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
 	console.log(store.getState());
 });
 
-store.dispatch({
-	type: "INCREMENT",
-	incrementBy: 5,
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-	type: "INCREMENT",
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-	type: "RESET",
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-	type: "DECREMENT",
-	decrementBy: 10,
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-	type: "SET",
-	count: 101,
-});
+store.dispatch(setCount({ count: 101 }));
---
+++ expensify-app/src/playground/redux-expensify.js
@@ -0,0 +1,99 @@
+import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
+
+//ADD_EXPENSE
+
+const addExpense = ({
+	description = "",
+	note = "",
+	amount = 0,
+	createdAt = 0,
+} = {}) => ({
+	type: "ADD_EXPENSE",
+	expense: {
+		id: uuid(),
+		description,
+		note,
+		amount,
+		createdAt,
+	},
+});
+
+//Remove_EXPENSE
+
+const removeExpense = ({ id } = {}) => ({
+	type: "REMOVE_EXPENSE",
+	id,
+});
+
+//Edit_expense
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_END_DATE
+//SET_START_DATE
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+	switch (action.type) {
+		case "ADD_EXPENSE":
+			return [...state, action.expense];
+		case "REMOVE_EXPENSE":
+			return state.filter(({ id }) => id !== action.id);
+		default:
+			return state;
+	}
+};
+
+const filtersReducerDefaultState = {
+	text: "rent",
+	sortBy: "amount", //date or amount
+	startDate: undefined,
+	endDate: undefined,
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+const store = createStore(
+	combineReducers({
+		expenses: expensesReducer,
+		filters: filtersReducer,
+	})
+);
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(
+	addExpense({ description: "Rent", amount: 100 })
+);
+const expenseTwo = store.dispatch(
+	addExpense({ description: "Coffee", amount: 300 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+const demoState = {
+	expenses: [
+		{
+			id: "trehfghf",
+			description: "January Rent",
+			note: "This was the final payment for that address",
+			amount: 54500,
+			createdAt: 0,
+		},
+	],
+	filters: {
+		text: "rent",
+		sortBy: "amount", //date or amount
+		startDate: undefined,
+		endDate: undefined,
+	},
+};
