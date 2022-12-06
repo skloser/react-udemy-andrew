bcef45a8282d0908e6d4f46b14eb72d787c9939e
Krum Bagashev <krum.bagashev@pharmalex.com>
2022-12-06 13:47:16 +0200

    Added express and updates for webpack scss and .js

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,34 +1,38 @@
-//entry -> output
-
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-	entry: "./src/app.js",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js",
-	},
-	module: {
-		rules: [
-			{
-				loader: "babel-loader",
-				test: /\.js$/,
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.s?css$/,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			},
-		],
-	},
-	devtool: "eval-cheap-module-source-map",
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "public"),
+module.exports = (env) => {
+	const isProduction = env.production;
+
+	return {
+		entry: "./src/app.js",
+		output: {
+			path: path.join(__dirname, "public"),
+			filename: "bundle.js",
 		},
-		historyApiFallback: true,
-		compress: true,
-		port: 9000,
-	},
-	mode: "development",
+		module: {
+			rules: [
+				{
+					loader: "babel-loader",
+					test: /\.js$/,
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.s?css$/,
+					use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+				},
+			],
+		},
+		plugins: [new MiniCssExtractPlugin()],
+		devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
+		devServer: {
+			static: {
+				directory: path.join(__dirname, "public"),
+			},
+			historyApiFallback: true,
+			compress: true,
+			port: 9000,
+		},
+		mode: "development",
+	};
 };
